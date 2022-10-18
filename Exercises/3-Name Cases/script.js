2973ae4ce72d223bea0df8ebc26c30f921c72ca6
rajaadeeltalib <114896091+rajaadeeltalib@users.noreply.github.com>
2022-10-18 09:10:28 +0500

    Update script.js
    Code Updated

--- Exercises/3-Name Cases/script.js
+++ Exercises/3-Name Cases/script.js
@@ -1,11 +1,16 @@
-
 //Excerise No. 3. 
 
-var nameCases = "Malik Naseer Ahmed"
+var nameCases = "malik Naseer AHMED"
 
 console.log(nameCases.toLowerCase());
 
 console.log(nameCases.toUpperCase());
 
-console.log(nameCases);
-
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+  console.log(titleCase("Malik naseer AHMED"));
